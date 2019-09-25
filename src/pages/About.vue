b3d5bc35753943ce571100ff2241adcb0aaf35f4
Lluís Peinado <lpeinado@stelladot.com>
2019-09-25 18:13:21 +0200

    Preliminar commit on marfan.cat

---
+++ src/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <h1>Qui som?</h1>
+    <p>
+      Els afectats per la Síndrome de Marfan a Catalunya fa anys que ens hem associat i vam crear aquesta agrupació per tal de millorar
+      la nostra qualitat de vida i compartir tota la informació necessària per tirar endevant. 
+    </p>
+    <p>
+      La Síndrome de Marfan és una malaltia minoritària i ens cal estar units i compartir tots els coneixements que anem acumulant
+      per tal de fer-nos costat els uns als altres.
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Qui som?'
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,51 @@
+<!--
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>Hello, world!</h1>
+   
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+-->
+<template>
+  <Layout>
+    <p>
+      Benvinguts al web de l'Associació d'Afectats de la Síndrome de Marfan a Catalunya.
+    </p>
+    <p>    
+      Aquest web està ara com ara en construcció i el farem crèixer poc a poc per tal de poder-vos oferir tota la informació
+      relacionada amb la síndrome i també amb els seus pacients i familiars :-)
+    </p>
+    <p>    
+      Farem aquest cami junts.
+    </p>
+
+  </Layout>
+</template>
+<script>
+export default {
+  metaInfo: {
+    title: "Pagina de benvinguda del web de l'Associació d'Afectats de la Síndrome de Marfan a Catalunya"
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
+
+
