eb0554e752f87b8474480d3452dd01dc291a2ef5
Lluís Peinado <lpeinado@stelladot.com>
2019-09-26 15:25:21 +0200

    0001-Test page commit

---
+++ src/pages/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Pàgina de prova</h1>
+    <p>De moment, això és només una secció de prova dins el nostre lloc web :-D</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Pàgina de prova"
+  }
+};
+</script>
